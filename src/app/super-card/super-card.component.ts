9b02eb62a9e2833f5cc1a44313cc3869f8af426f
mmadsen <mmadsen@inmoment.com>
2019-09-12 21:51:23 -0600

    Inputs

--- src/app/super-card/super-card.component.ts
+++ src/app/super-card/super-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-super-card',
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./super-card.component.scss']
 })
 export class SuperCardComponent implements OnInit {
+  @Input() title: string;
+  @Input() imageSrc: string;
+
+  @Input('titleImageSrc')
+  set titleImageSrc(value: string) {
+    this._titleImageSrc = `url(${value})`;
+  }
+  get titleImageSrc(): string {
+    return this._titleImageSrc;
+  }
+
+  private _titleImageSrc: string;
 
   constructor() { }
 
